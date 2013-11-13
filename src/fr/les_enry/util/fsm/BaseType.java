e57a7310a0ced4c966cf0eff5b379018820b21d1
Eric Mountain <eric.stephen.mountain@gmail.com>
2013-11-13 23:11:09 +0100

    Remove already commented-out code regarding id and lastId in BaseType.

--- src/fr/les_enry/util/fsm/BaseType.java
+++ src/fr/les_enry/util/fsm/BaseType.java
@@ -7,13 +7,9 @@ abstract class BaseType implements Serializable {
 
 	private static final long serialVersionUID = -5901944619472256851L;
 
-//	private static int lastId = 0;
-	
-//	private final int id;
 	private String name;
 	
 	BaseType() {
-//		id = ++lastId;
 		name = "not set";
 	}
 	
@@ -24,13 +20,11 @@ abstract class BaseType implements Serializable {
 
 	@Override
 	public boolean equals(Object obj) {
-//		return id == ((BaseType) obj).id;
 		return name.equals(((BaseType) obj).name);
 	}
 
 	@Override
 	public int hashCode() {
-//		return id;
 		return name.hashCode();
 	}
 
